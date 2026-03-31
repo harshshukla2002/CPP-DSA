1cc6c7d71f916c749d2fc7970c15584ec888f238
Harsh Shukla <harshshukla@MacBook-Air-od-Harsh.local>
2026-03-31 16:13:48 +0530

    learned about Linked List

---
+++ linked list/reverse-ll.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+ListNode *reverseLinkedList(ListNode *head)
+{
+    LiseNode *prev = NULL;
+    LiseNode *next = NULL;
+    LiseNode *curr = head;
+
+    while (curr != NULL)
+    {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+
+    return prev;
+}
+
+int main()
+{
+    cout << "Boiler Plate Code" << endl;
+    return 0;
+}
