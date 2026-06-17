62fa6797bc5d356a69fc218d09ac0868f6c17f2c
Harsh Shukla <harshshukla2312@gmail.com>
2026-06-17 22:12:47 +0530

    solved questions

---
+++ leetcode/max-consecutive-ones.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+   public:
+    int longestOnes(vector<int>& nums, int k) {
+        int left = 0;
+        int zeroCount = 0;
+        int ans = 0;
+
+        for (int right = 0; right < nums.size(); right++) {
+            if (nums[right] == 0)
+                zeroCount++;
+
+            while (zeroCount > k) {
+                if (nums[left] == 0)
+                    zeroCount--;
+
+                left++;
+            }
+
+            ans = max(ans, right - left + 1);
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    cout << "Boiler Plate Code" << endl;
+    return 0;
+}
