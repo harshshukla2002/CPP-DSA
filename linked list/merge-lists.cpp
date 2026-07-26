4061db3d8a0d3286a3b6ed7bbca7bb676518fa79
Harsh Shukla <harshshukla2312@gmail.com>
2026-07-26 16:34:58 +0530

    solved questions

--- linked list/merge-lists.cpp
+++ linked list/merge-lists.cpp
@@ -1,27 +1,21 @@
 #include <iostream>
 using namespace std;
 
-ListNode *mergeList(ListNode *head1, ListNode *head2)
-{
-    if (head1 == NULL || head2 == NULL)
-    {
+ListNode* mergeList(ListNode* head1, ListNode* head2) {
+    if (head1 == NULL || head2 == NULL) {
         return head1 == NULL ? head2 : head1;
     }
 
-    if (head1->val <= head2->val)
-    {
+    if (head1->val <= head2->val) {
         head1->next = mergeList(head1->next, head2);
         return head1;
-    }
-    else
-    {
+    } else {
         head2->next = mergeList(head1, head2->next);
         return head2;
     }
 }
 
-int main()
-{
+int main() {
     cout << "Boiler Plate Code" << endl;
     return 0;
 }
